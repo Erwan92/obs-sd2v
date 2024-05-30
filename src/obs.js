d6e33ebdd6f55f20c76cb0c08b6568d9c1c21382
Erwan92 <Erwan92@users.noreply.github.com>
2024-05-30 10:36:31 +0200

    Update obs.js

--- src/obs.js
+++ src/obs.js
@@ -1,11 +1,11 @@
-import OBSWebSocket from 'obs-websocket-js'
+ import OBSWebSocket from 'obs-websocket-js'
 
 export const obs = new OBSWebSocket()
 
 export async function sendCommand (command, params) {
   try {
     // if (command.indexOf('Set') === 0)
-    //  console.log('Sending command:', command, 'with params:', params)
+    //  console.log('Envoi de la commande:', command, 'avec les paramètres:', params)
     return await obs.call(command, params || {})
   } catch (e) {
     console.log('Error sending command', command, ' - error is:', e.message)
